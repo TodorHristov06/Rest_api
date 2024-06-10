ad14ed5f0b8eaade2cf062976ce58ad0936919d6
Тодор Христов <todorhristov2006@gmail.com>
2024-06-10 21:32:06 +0300

    Update settings

--- desktop_bg_api/api/api/settings.py
+++ desktop_bg_api/api/api/settings.py
@@ -73,10 +73,16 @@ WSGI_APPLICATION = 'api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'path/to/computers.db',   #desktop_bg_api\api\computers.db
     }
 }
 
