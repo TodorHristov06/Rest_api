f13aeb72ea6d9dbd3932c9c79a24376c6c2c0741
Тодор Христов <todorhristov2006@gmail.com>
2024-06-10 21:32:19 +0300

    Update models

--- desktop_bg_api/api/computers/models.py
+++ desktop_bg_api/api/computers/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+class Computer(models.Model):
+    url = models.URLField(unique=True)
+    processor = models.CharField(max_length=100)
+    gpu = models.CharField(max_length=100)
+    motherboard = models.CharField(max_length=100)
+    ram = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return f"{self.processor} | {self.gpu} | {self.motherboard} | {self.ram}"
+
 # Create your models here.
